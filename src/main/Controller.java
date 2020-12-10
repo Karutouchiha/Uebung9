afe454d4c17e4455e25d4bc94e7910d526ff4c2d
Sarah Haboeck <sarah.haboeck@htl-wels.at>
2020-12-10 13:25:36 +0100

    2. Oberfläche

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -4,15 +4,36 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 
+import java.util.Stack;
+
 
 public class Controller {
 
+    private Stack<Double> stack = new Stack();
+
     @FXML
-    private void clickButton(MouseEvent event) {
+    private void clickNummberblock(MouseEvent event) {
         Object node = event.getSource(); //returns the object that generated the event
         //since the returned object is a Button we can do a typecast
         Button btn = (Button)node;
         String s = btn.getText();
-        System.out.println(s);
+        if (s.equals(".")){
+            try {
+                stack.push(Double.parseDouble(stack.pop() + "."));
+            }
+            catch (Exception ex){
+
+            }
+        }
+        else if (s.equals("-")) {
+
+        }
+        stacktoString();
+    }
+    private void stacktoString(){
+        System.out.println("Stack");
+        for (int i=0; i<stack.size();i++){
+            System.out.println(i+":"+stack.get(i));
+        }
     }
 }
